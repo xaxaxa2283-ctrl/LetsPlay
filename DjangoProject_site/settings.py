45f1737788f2895a6b8db0fc71d7341d8b2ddbe4
xaxaxa2283-ctrl <xaxaxa2283@gmail.com>
2025-10-27 10:51:40 +0500

    migrate db to postgres

--- DjangoProject_site/settings.py
+++ DjangoProject_site/settings.py
@@ -107,9 +107,7 @@ WSGI_APPLICATION = 'DjangoProject_site.wsgi.application'
 
 
 DATABASES = {
-    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'),
-    conn_max_age=600,
-    ssl_require=True
+    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL')
                     )
 }
 
